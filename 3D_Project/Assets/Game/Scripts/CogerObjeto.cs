7806e31f4efed416da9d1d0970435f4242616279
Sun-Jinwoo <juan_camilo.tenorio@uao.edu.co>
2025-11-05 19:30:22 -0500

    Update_PJ

--- 3D_Project/Assets/Game/Scripts/CogerObjeto.cs
+++ 3D_Project/Assets/Game/Scripts/CogerObjeto.cs
@@ -1,44 +1,38 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem; // ← ESTO ES LO ÚNICO NUEVO
 
 public class CogerObjeto : MonoBehaviour
 {
-
     public GameObject handPoint;
     private GameObject pickedObject = null;
-    // Update is called once per frame
+
     void Update()
     {
-        
-        if (pickedObject != null)
+        // SOLTAR con Q
+        if (pickedObject != null && Keyboard.current.qKey.isPressed)
         {
-            if (Input.GetKey("q"))
-            {
-                pickedObject.GetComponent<Rigidbody>().useGravity = true;
-                pickedObject.GetComponent<Rigidbody>().isKinematic = false;
-                pickedObject.transform.SetParent(null);
-                pickedObject = null;
-            }
+            Rigidbody rb = pickedObject.GetComponent<Rigidbody>();
+            rb.useGravity = true;
+            rb.isKinematic = false;
+            pickedObject.transform.SetParent(null);
+            pickedObject = null;
         }
-
-
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("ObjetoAgarrable"))
+        if (other.CompareTag("ObjetoAgarrable"))
         {
-            if (Input.GetKey("e")) { 
-                other.GetComponent<Rigidbody>().useGravity = false;
-
-                other.GetComponent<Rigidbody>().useGravity = true;
-
+            // AGARRAR con E (solo una vez al pulsar)
+            if (Keyboard.current.eKey.wasPressedThisFrame)
+            {
+                Rigidbody rb = other.GetComponent<Rigidbody>();
+                rb.useGravity = false;
+                rb.isKinematic = true;
                 other.transform.position = handPoint.transform.position;
-
-                other.gameObject.transform.SetParent(handPoint.gameObject.transform);
-
+                other.transform.SetParent(handPoint.transform);
                 pickedObject = other.gameObject;
             }
         }
     }
-
 }
